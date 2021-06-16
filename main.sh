1916e94b5ef63fb6d3d3e950aa8cbf33ff89a2a2
Jake Wnuk <jakewnuk@gmail.com>
2021-06-16 15:18:06 -0400

    update zshrc and zip wordlists
    final update to zshrc and zipped wordlists for faster clone

--- main.sh
+++ main.sh
@@ -70,7 +70,7 @@ git clone https://github.com/blechschmidt/massdns.git /tmp/massdns && cd /tmp/ma
 wget -O /tmp/aquatone.zip https://github.com/michenriksen/aquatone/releases/download/v1.7.0/aquatone_linux_amd64_1.7.0.zip && cd /tmp/ && sudo unzip /tmp/aquatone.zip && sudo mv /tmp/aquatone $HOME/go/bin/aquatone
 wget -O /tmp/amass.zip https://github.com/OWASP/Amass/releases/download/v3.11.1/amass_linux_amd64.zip && cd /tmp/ && sudo unzip /tmp/amass.zip && sudo mv /tmp/amass_linux_amd64/amass $HOME/go/bin/amass
 wget -O /tmp/kiterunner.tar.gz https://github.com/assetnote/kiterunner/releases/download/v1.0.2/kiterunner_1.0.2_linux_amd64.tar.gz && cd /tmp/ && sudo tar -xf kiterunner.tar.gz && sudo mv ./kr /bin/kr
-sudo mkdir /usr/share/zsh-z && wget -O /usr/share/zsh-z/zsh-z.plugin.zsh https://raw.githubusercontent.com/agkozak/zsh-z/master/zsh-z.plugin.zsh
+mkdir ~/.zsh/; wget -O ~/.zsh/zsh-z.plugin.zsh https://raw.githubusercontent.com/agkozak/zsh-z/master/zsh-z.plugin.zsh
 
 # install Wordlists
 echo 'Installing Wordlists'
